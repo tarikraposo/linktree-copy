c6c4ed68c84ae7d4bd76e8cb302d625431914ee3
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2025-05-05 18:34:41 -0300

    Feat: Cria componente de links.

---
+++ src/app/components/link/link.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-link',
+  imports: [],
+  templateUrl: './link.component.html',
+  styleUrl: './link.component.scss'
+})
+export class LinkComponent {
+
+}
