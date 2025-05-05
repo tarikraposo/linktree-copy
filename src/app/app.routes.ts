c464be8e3062396c7be0f3822cd2b803f8198644
Tarik Raposo Parreira <tarik.parreira@mpdft.mp.br>
2025-05-05 18:33:19 -0300

    Feat: cria rota para a pag home.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
+import { HomeComponent } from './pages/home/home.component';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '',
+    component: HomeComponent
+  }
+];
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { LinkComponent } from "../../components/link/link.component";
+
+@Component({
+  selector: 'app-home',
+  imports: [LinkComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+
+}
